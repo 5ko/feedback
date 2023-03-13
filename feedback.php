a89a3d0f07568e8c079b818a377a378690f8e6c7
Petko Yotov <5ko@5ko.fr>
2023-03-13 12:19:00 +0100

    Create feedback.php

---
+++ feedback.php
@@ -0,0 +1,12 @@
+<?php if (!defined('PmWiki')) exit();
+/**
+  Feedback: Accept messages from visitors to PmWiki pages
+  Written by (c) Petko Yotov 2023   www.pmwiki.org/Petko
+
+  This text is written for PmWiki; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published
+  by the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version. See pmwiki.php for full details
+  and lack of warranty.
+*/
+$RecipeInfo['Feedback']['Version'] = '20230313';
